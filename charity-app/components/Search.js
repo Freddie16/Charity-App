b7c0958ae9aa77ac7fc8aac0abbb80427cd124b0
Fred Murigi <105373496+Freddie16@users.noreply.github.com>
2023-01-26 12:31:45 +0300

    Added the commits

---
+++ charity-app/components/Search.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const SearchBar = ({keyword, onChange}) => {
+    const BarStyle = {width:"20rem",background:"#F0F0F0", border:"solid", padding:"0.5rem"};
+    return (
+      <input 
+       style={BarStyle}
+       key="search-bar"
+       value={keyword}
+       placeholder={"SEARCH FUND"}
+       onChange={(e) => onChange(e.target.value)}
+      />
+    );
+  }
+  
+  export default SearchBar;
