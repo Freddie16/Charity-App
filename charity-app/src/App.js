431cd61db9a0e58ef6b35763e0e90d152ce2d3a5
Fred Murigi <freddiemurigi@gmail.com>
2023-01-26 11:02:51 +0300

    added the commits

---
+++ charity-app/src/App.js
@@ -0,0 +1,53 @@
+
+
+import React from 'react'
+import { BrowserRouter as Router,Routes, Route } from 'react-router-dom'
+import LandingPage from './components/LandingPage'
+import LoginPage from './components/LoginPage'
+import RegisterPage from './components/RegisterPage'
+import ForgetPasswordPage from './components/ForgetPasswordPage'
+import HomePage from './components/Homepage'
+import SearchBar from './components/Search'
+
+import './App.css'
+
+
+
+
+
+export default function App() {
+    return (
+        <Router>
+            <div>
+                <Routes>
+                    <Route path="/search" element={< SearchBar/> } />
+                    <Route path="/" element={ <LandingPage/> } />
+                    <Route path="/login" element={ <LoginPage/> } />
+                    <Route path="/register" element={ <RegisterPage />} />
+                    <Route path="/forget-password" element={ <ForgetPasswordPage />} />
+                    <Route path="/home" element={< HomePage/> } />
+                </Routes>
+                <Footer />
+            </div>
+        </Router>
+    )
+}
+
+const Footer = () => {
+    return (
+        <p className="text-center" style={ FooterStyle }>Charity app</p>
+    )
+}
+
+const FooterStyle = {
+    background: "#222",
+    fontSize: ".8rem",
+    color: "#fff",
+    position: "absolute",
+    bottom: 0,
+    padding: "1rem",
+    margin: 0,
+    width: "100%",
+    opacity: ".5"
+}
+
